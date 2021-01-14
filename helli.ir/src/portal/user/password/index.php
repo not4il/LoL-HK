309fc46d6f1422df91cfd31500389acb8624a9f8
Amir R <61323886+weed-web@users.noreply.github.com>
2021-01-14 13:07:07 +0330

    Create index.php

---
+++ helli.ir/src/portal/user/password/index.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html lang="fa" dir="rtl" prefix="content: http://purl.org/rss/1.0/modules/content/ dc: http://purl.org/dc/terms/ foaf: http://xmlns.com/foaf/0.1/ og: http://ogp.me/ns# rdfs: http://www.w3.org/2000/01/rdf-schema# sioc: http://rdfs.org/sioc/ns# sioct: http://rdfs.org/sioc/types# skos: http://www.w3.org/2004/02/skos/core# xsd: http://www.w3.org/2001/XMLSchema#">
+<head>
+  <link rel="profile" href="http://www.w3.org/1999/xhtml/vocab" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="shortcut icon" href="https://www.helli.ir/portal/sites/default/files/sampad.ico" type="image/vnd.microsoft.icon" />
+<meta name="Generator" content="Drupal 7 (http://drupal.org)" />
+  <title>حساب کاربری | دبیرستان علامه حلی تهران</title>
+  <style>
+@import url("https://www.helli.ir/portal/modules/system/system.base.css");
+@import url("https://www.helli.ir/portal/modules/system/system.base-rtl.css");
+</style>
+<style>
+@import url("https://www.helli.ir/portal/sites/all/libraries/slick/slick/slick.css");
+</style>
+<style>
+@import url("https://www.helli.ir/portal/sites/all/modules/simplenews/simplenews.css");
+@import url("https://www.helli.ir/portal/sites/all/modules/counter/counter.css");
+@import url("https://www.helli.ir/portal/sites/all/modules/date/date_api/date.css");
+@import url("https://www.helli.ir/portal/sites/all/modules/date/date_api/date-rtl.css");
+@import url("https://www.helli.ir/portal/sites/all/modules/date/date_popup/themes/datepicker.1.7.css");
+@import url("https://www.helli.ir/portal/modules/field/theme/field.css");
+@import url("https://www.helli.ir/portal/modules/field/theme/field-rtl.css");
+@import url("https://www.helli.ir/portal/modules/node/node.css");
+@import url("https://www.helli.ir/portal/sites/all/modules/quiz/quiz.css");
+@import url("https://www.helli.ir/portal/sites/all/modules/workflow/workflow_admin_ui/workflow_admin_ui.css");
+@import url("https://www.helli.ir/portal/sites/all/modules/views/css/views.css");
+@import url("https://www.helli.ir/portal/sites/all/modules/views/css/views-rtl.css");
+@import url("https://www.helli.ir/portal/sites/all/modules/media/modules/media_wysiwyg/css/media_wysiwyg.base.css");
+</style>
+<style>
+@import url("https://www.helli.ir/portal/sites/all/libraries/bootstrap/css/bootstrap-theme.min.css");
+@import url("https://www.helli.ir/portal/sites/all/modules/ctools/css/ctools.css");
+@import url("https://www.helli.ir/portal/sites/all/modules/panels/css/panels.css");
+@import url("https://www.helli.ir/portal/sites/all/modules/rate/rate.css");
+@import url("https://www.helli.ir/portal/sites/all/modules/uif/uif.css");
+</style>
+<link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.css" media="all" />
+<link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@unicorn-fail/drupal-bootstrap-styles@0.0.2/dist/3.3.1/7.x-3.x/drupal-bootstrap.css" media="all" />
+<style>
+@import url("https://www.helli.ir/portal/sites/all/themes/helli/css/bootstrap.min.css");
+@import url("https://www.helli.ir/portal/sites/all/themes/helli/css/bootstrapRTL.min.css");
+@import url("https://www.helli.ir/portal/sites/all/themes/helli/css/animate.css");
+@import url("https://www.helli.ir/portal/sites/all/themes/helli/css/owl.carousel.css");
+@import url("https://www.helli.ir/portal/sites/all/themes/helli/css/easyzoom.css");
+@import url("https://www.helli.ml/portal/sites/all/themes/helli/css/style.css");
+@import url("https://www.helli.ir/portal/sites/default/files/color/helli-96e729f2/colors.css");
+</style>
+  <!-- HTML5 element support for IE6-8 -->
+  <!--[if lt IE 9]>
+    <script src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv-printshiv.min.js"></script>
+  <![endif]-->
+  <script src="https://www.helli.ir/portal/sites/all/modules/jquery_update/replace/jquery/2.1/jquery.min.js?v=2.1.4"></script>
+<script src="https://www.helli.ir/portal/misc/jquery-extend-3.4.0.js?v=2.1.4"></script>
+<script src="https://www.helli.ir/portal/misc/jquery.once.js?v=1.2"></script>
+<script src="https://www.helli.ir/portal/misc/drupal.js"></script>
+<script src="https://www.helli.ir/portal/sites/all/libraries/slick/slick/slick.min.js?v=1.x"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.js"></script>
+<script src="https://www.helli.ir/portal/sites/all/modules/webform_steps/webform_steps.js"></script>
+<script src="https://www.helli.ir/portal/sites/default/files/languages/fa_Mpwv7GX5hI8YSlol4zOkgxwnoz7UeKBcX4z4NgV-pt8.js"></script>
+<script src="https://www.helli.ir/portal/sites/all/libraries/bootstrap/js/bootstrap.min.js"></script>
+<script src="https://www.helli.ir/portal/sites/all/modules/google_analytics/googleanalytics.js"></script>
+<script src="https://www.helli.ir/portal/sites/all/modules/slick/js/slick.load.min.js"></script>
+<script>(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,"script","https://www.google-analytics.com/analytics.js","ga");ga("create", "UA-28952410-1", {"cookieDomain":"auto"});ga("send", "pageview");</script>
+<script src="https://www.helli.ir/portal/sites/all/themes/helli/js/bootstrap.min.js"></script>
+<script src="https://www.helli.ir/portal/sites/all/themes/helli/js/wow.min.js"></script>
+<script src="https://www.helli.ir/portal/sites/all/themes/helli/js/owl.carousel.min.js"></script>
+<script src="https://www.helli.ir/portal/sites/all/themes/helli/js/easyzoom.js"></script>
+<script src="https://www.helli.ir/portal/sites/all/themes/helli/js/scripts.js"></script>
+<script>jQuery.extend(Drupal.settings, {"basePath":"\/portal\/","pathPrefix":"","ajaxPageState":{"theme":"helli","theme_token":"HSo4mjd32lcayXHzGMIz-y8CZImTNyItlXsM4rrNYHk","js":{"sites\/all\/themes\/bootstrap\/js\/bootstrap.js":1,"sites\/all\/modules\/jquery_update\/replace\/jquery\/2.1\/jquery.min.js":1,"misc\/jquery-extend-3.4.0.js":1,"misc\/jquery.once.js":1,"misc\/drupal.js":1,"sites\/all\/libraries\/slick\/slick\/slick.min.js":1,"https:\/\/cdn.jsdelivr.net\/npm\/bootstrap@3.3.7\/dist\/js\/bootstrap.js":1,"sites\/all\/modules\/webform_steps\/webform_steps.js":1,"public:\/\/languages\/fa_Mpwv7GX5hI8YSlol4zOkgxwnoz7UeKBcX4z4NgV-pt8.js":1,"sites\/all\/libraries\/bootstrap\/js\/bootstrap.min.js":1,"sites\/all\/modules\/google_analytics\/googleanalytics.js":1,"sites\/all\/modules\/slick\/js\/slick.load.min.js":1,"0":1,"sites\/all\/themes\/helli\/js\/bootstrap.min.js":1,"sites\/all\/themes\/helli\/js\/wow.min.js":1,"sites\/all\/themes\/helli\/js\/owl.carousel.min.js":1,"sites\/all\/themes\/helli\/js\/easyzoom.js":1,"sites\/all\/themes\/helli\/js\/scripts.js":1},"css":{"modules\/system\/system.base.css":1,"modules\/system\/system.base-rtl.css":1,"sites\/all\/libraries\/slick\/slick\/slick.css":1,"sites\/all\/modules\/simplenews\/simplenews.css":1,"sites\/all\/modules\/counter\/counter.css":1,"sites\/all\/modules\/date\/date_api\/date.css":1,"sites\/all\/modules\/date\/date_api\/date-rtl.css":1,"sites\/all\/modules\/date\/date_popup\/themes\/datepicker.1.7.css":1,"modules\/field\/theme\/field.css":1,"modules\/field\/theme\/field-rtl.css":1,"modules\/node\/node.css":1,"sites\/all\/modules\/quiz\/quiz.css":1,"sites\/all\/modules\/workflow\/workflow_admin_ui\/workflow_admin_ui.css":1,"sites\/all\/modules\/views\/css\/views.css":1,"sites\/all\/modules\/views\/css\/views-rtl.css":1,"sites\/all\/modules\/media\/modules\/media_wysiwyg\/css\/media_wysiwyg.base.css":1,"sites\/all\/libraries\/bootstrap\/css\/bootstrap.min.css":1,"sites\/all\/libraries\/bootstrap\/css\/bootstrap-theme.min.css":1,"sites\/all\/modules\/ctools\/css\/ctools.css":1,"sites\/all\/modules\/panels\/css\/panels.css":1,"sites\/all\/modules\/rate\/rate.css":1,"sites\/all\/modules\/uif\/uif.css":1,"https:\/\/cdn.jsdelivr.net\/npm\/bootstrap@3.3.7\/dist\/css\/bootstrap.css":1,"https:\/\/cdn.jsdelivr.net\/npm\/@unicorn-fail\/drupal-bootstrap-styles@0.0.2\/dist\/3.3.1\/7.x-3.x\/drupal-bootstrap.css":1,"sites\/all\/themes\/helli\/css\/bootstrap.min.css":1,"sites\/all\/themes\/helli\/css\/bootstrapRTL.min.css":1,"sites\/all\/themes\/helli\/css\/animate.css":1,"sites\/all\/themes\/helli\/css\/owl.carousel.css":1,"sites\/all\/themes\/helli\/css\/easyzoom.css":1,"sites\/all\/themes\/helli\/css\/style.css":1,"sites\/all\/themes\/helli\/css\/colors.css":1}},"slick":{"accessibility":true,"adaptiveHeight":false,"autoplay":false,"autoplaySpeed":3000,"pauseOnHover":true,"pauseOnDotsHover":false,"arrows":true,"centerMode":false,"centerPadding":"50px","dots":false,"dotsClass":"slick-dots","draggable":true,"fade":false,"focusOnSelect":false,"infinite":true,"initialSlide":0,"lazyLoad":"ondemand","mousewheel":false,"randomize":false,"rtl":false,"rows":1,"slidesPerRow":1,"slide":"","slidesToShow":1,"slidesToScroll":1,"speed":500,"swipe":true,"swipeToSlide":false,"edgeFriction":0.35,"touchMove":true,"touchThreshold":5,"useCSS":true,"cssEase":"ease","useTransform":true,"easing":"linear","variableWidth":false,"vertical":false,"verticalSwiping":false,"waitForAnimate":true},"googleanalytics":{"trackOutbound":1,"trackMailto":1,"trackDownload":1,"trackDownloadExtensions":"7z|aac|arc|arj|asf|asx|avi|bin|csv|doc(x|m)?|dot(x|m)?|exe|flv|gif|gz|gzip|hqx|jar|jpe?g|js|mp(2|3|4|e?g)|mov(ie)?|msi|msp|pdf|phps|png|ppt(x|m)?|pot(x|m)?|pps(x|m)?|ppam|sld(x|m)?|thmx|qtm?|ra(m|r)?|sea|sit|tar|tgz|torrent|txt|wav|wma|wmv|wpd|xls(x|m|b)?|xlt(x|m)|xlam|xml|z|zip"},"urlIsAjaxTrusted":{"\/portal\/user\/password":true},"bootstrap":{"anchorsFix":"0","anchorsSmoothScrolling":"0","formHasError":1,"popoverEnabled":1,"popoverOptions":{"animation":1,"html":0,"placement":"right","selector":"","trigger":"click","triggerAutoclose":1,"title":"","content":"","delay":0,"container":"body"},"tooltipEnabled":1,"tooltipOptions":{"animation":1,"html":0,"placement":"auto left","selector":"","trigger":"hover focus","delay":0,"container":"body"}}});</script>
+</head>
+<body class="html not-front not-logged-in no-sidebars page-user page-user-password">
+  <div id="skip-link">
+    <a href="#main-content" class="element-invisible element-focusable">انتقال به محتوای اصلی</a>
+  </div>
+    <div class="bg">
+    <div id="branding">
+        <div class="container-fluid">
+            <div class="row mnudesktop">
+                <div class="col-md-2 col-sm-2"><img class="img-responsive" src="https://helli.ir/portal/sites/all/themes/helli/image/headerRR.jpg"></div>
+				<div class="col-md-2 col-sm-2"><img class="img-responsive" src="https://helli.ir/portal/sites/all/themes/helli/image/headerR.jpg"></div>
+                <div class="col-md-4 col-sm-4"><img class="img-responsive" src="https://helli.ir/portal/sites/all/themes/helli/image/headerCenter.jpg"></div>
+				<div class="col-md-2 col-sm-2"><img class="img-responsive" src="https://helli.ir/portal/sites/all/themes/helli/image/headerL.jpg"></div>
+				<div class="col-md-2 col-sm-2"><img class="img-responsive" src="https://helli.ir/portal/sites/all/themes/helli/image/headerLL.jpg"></div>
+            </div>
+            <div class="mnumobile">
+                <div class="row">
+                <div class="col-xs-3"><img class="img-responsive" src="https://helli.ir/portal/sites/all/themes/helli/image/headerRR.jpg"></div>
+                <div class="col-xs-6"><img class="img-responsive" src="https://helli.ir/portal/sites/all/themes/helli/image/headerCenter.jpg"></div>
+				<div class="col-xs-3"><img class="img-responsive" src="https://helli.ir/portal/sites/all/themes/helli/image/headerLL.jpg"></div>    
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="nav">
+    <header id="navbar" role="banner" class="navbar container navbar-default">
+  <div class="container">
+    <div class="navbar-header">
+      
+      
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+          </div>
+
+          <div class="navbar-collapse collapse" id="navbar-collapse">
+        <nav role="navigation">
+                      <ul class="menu nav navbar-nav"><li class="first leaf"><a href="/portal/">خانه</a></li>
+<li class="leaf"><a href="/portal/hallofhonors" title="">تالار افتخارات</a></li>
+<li class="collapsed"><a href="/portal/about">درباره ما</a></li>
+<li class="collapsed"><a href="/portal/content/%D9%85%D8%AF%DB%8C%D8%B1%DB%8C%D8%AA" title="">مدیریت</a></li>
+<li class="collapsed"><a href="/portal/amoozeshi" title="">معاونت آموزشی</a></li>
+<li class="collapsed"><a href="/portal/pajouheshi">معاونت پژوهشی</a></li>
+<li class="collapsed"><a href="/portal/tarbiyati">معاونت تربیتی</a></li>
+<li class="collapsed"><a href="/portal/amoozeshi/olympiad">المپیاد</a></li>
+<li class="leaf active-trail active"><a href="/portal/user?current=user/password" title="" class="active-trail active">کاربران</a></li>
+<li class="leaf"><a href="/portal/news" title="">اخبار</a></li>
+<li class="leaf"><a href="https://www.helli.ml/ids" title="">سامانه سفیر</a></li>
+<li class="last collapsed"><a href="/portal/hellisanj">حلی سنج</a></li>
+</ul>                                      </nav>
+      </div>
+      </div>
+        </header></div>
+</div>
+
+
+
+<div class="main-container userlogin container-fluid">
+
+  <header role="banner" id="page-header">
+    
+      </header> <!-- /#page-header -->
+
+  <div class="row">
+
+    
+    <section class="col-sm-12">
+            <ol class="breadcrumb"><li><a href="/portal/user?current=user/password" title="">حساب کاربری</a></li>
+<li class="active">درخواست رمز عبور جدید</li>
+</ol>      <a id="main-content"></a>
+                                <h2 class="element-invisible">تب های اصلی</h2><ul class="tabs--primary nav nav-tabs"><li><a href="/portal/user">ورود</a></li>
+<li class="active"><a href="/portal/user/password" class="active">درخواست رمز عبور جدید<span class="element-invisible">(تب فعال)</span></a></li>
+</ul>                          <div class="region region-content">
+    <section id="block-system-main" class="block block-system clearfix">
+
+      
+  <form action="/portal/user/password" method="post" id="user-pass" accept-charset="UTF-8"><div><div class="form-item form-item-name form-type-textfield form-group"> <label class="control-label" for="edit-name">نام کاربری یا آدرس ایمیل <span class="form-required" title="این فیلد اجباری می باشد.">*</span></label>
+<input class="form-control form-text required" type="text" id="edit-name" name="name" value="" size="60" maxlength="254" /></div><input type="hidden" name="form_build_id" value="form-RiSnpjKxBsQXJ7IlY0zTYyVJrFwwiGPSw92-i8XGQdI" />
+<input type="hidden" name="form_id" value="user_pass" />
+<div class="form-actions form-wrapper form-group" id="edit-actions"><button type="submit" id="edit-submit" name="op" value="ایمیل کردن رمز عبور جدید" class="btn btn-default form-submit">ایمیل کردن رمز عبور جدید</button>
+</div></div></form>
+</section>
+  </div>
+    </section>
+
+    
+  </div>
+</div>
+
+
+  <footer class="footer container">
+      <div class="row">
+          <div class="col-md-4 col-sm-12 logo">
+              <img class="img-responsive" src="https://www.helli.ir/portal/sites/all/themes/helli/image/Logo1.png"/>
+          </div>
+        <div class="col-md-4 col-sm-12 access">
+              <div class="region region-access">
+    <section id="block-block-9" class="block block-block clearfix">
+
+        <h2 class="block-title">دسترسی سریع</h2>
+    
+  <ul id="access"><li><a href="/portal/amoozeshi">معاونت آموزشی</a></li>
+<li><a href="/portal/pajooheshi">معاونت پژوهشی</a></li>
+<li><a href="/portal/tarbiyati">معاونت تربیتی</a></li>
+<li><a href="/portal/edari">معاونت اداری و مالی</a></li>
+</ul>
+</section>
+  </div>
+          </div>
+          <div class="col-md-4 col-sm-12 contact">
+                <div class="region region-contact">
+    <section id="block-block-10" class="block block-block clearfix">
+
+        <h2 class="block-title">ارتباط با ما</h2>
+    
+  <ul><li>تلفن: ۵۵۴۱۵۵۵۶ (۲۱) ۰۰۹۸</li>
+<li>دورنگار: ۵۵۴۰۹۳۵۴ (۲۱) ۰۰۹۸</li>
+<li>کدپستی: ۱۳۳۳۷۱۴۳۸۳</li>
+<li>پست الکترونیک : <a href="mailto:info@helli.ir">info@helli.ir</a></li>
+</ul>
+</section>
+  </div>
+          </div>
+      </div>
+      <div class="row">
+            
+      </div>
+      <div class="row">
+          <div class="col-md-6">
+                <div class="region region-footer">
+    <section id="block-block-3" class="block block-block clearfix">
+
+        <h2 class="block-title">دبیرستان علامه حلی تهران</h2>
+    
+  <div id="cr">
+<p>کلیه حقوق این سایت برای دبیرستان علامه حلی تهران محفوظ است.طراحی و توسعه <b><a href="http://elitec.ir" target="_blank">الیت</a></b></p>
+</div>
+
+</section>
+  </div>
+            </div>
+          <div class="wow animated fadeInRight col-md-6 social">
+            <a href="https://fa.wikipedia.org/wiki/%D8%AF%D8%A8%DB%8C%D8%B1%D8%B3%D8%AA%D8%A7%D9%86_%D8%B9%D9%84%D8%A7%D9%85%D9%87_%D8%AD%D9%84%DB%8C"><img src="https://www.helli.ir/portal/sites/all/themes/helli/image/wikipedia.png" class="img-responsive"></a>
+            <a href="https://www.linkedin.com/company/hellischool"><img src="https://www.helli.ir/portal/sites/all/themes/helli/image/linkedin.png" class="img-responsive"></a>
+            <a href="http://www.aparat.com/hellischool"><img src="https://www.helli.ir/portal/sites/all/themes/helli/image/aparat.png" class="img-responsive"></a>
+            <a href="https://www.instagram.com/hellischool"><img src="https://www.helli.ir/portal/sites/all/themes/helli/image/instagram.png" class="img-responsive"></a>
+            <a href="https://t.me/hellischool"><img src="https://www.helli.ir/portal/sites/all/themes/helli/image/telegram.png" class="img-responsive"></a>
+            <a href="http://www.lenzor.com/hellischool"><img src="https://www.helli.ir/portal/sites/all/themes/helli/image/lenzor.png" class="img-responsive"></a>
+            <a href="http://www.twitter.com/hellischool"><img src="https://www.helli.ir/portal/sites/all/themes/helli/image/twitter.png" class="img-responsive"></a>
+            <a href="http://www.facebook.com/hellischool"><img src="https://www.helli.ir/portal/sites/all/themes/helli/image/facebook.png" class="img-responsive"></a>
+          </div>
+        </div>
+  </footer>
+  <script src="https://www.helli.ir/portal/sites/all/themes/bootstrap/js/bootstrap.js"></script>
+</body>
+</html>
